89673bd80317f557934dc06fe9c631d9fb5554b8
ImmortalEnoch <151405103+ImmortalEnoch@users.noreply.github.com>
2023-11-18 22:30:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2348135176408', 'ImmortalEnoch', true],
+  ['2347059790748', 'I.E.O.', true], 
   [''] 
 ] //Number of owners
 
